114a8804d5d30fbd94eeb11feb5c9404a11f6017
Sahil Saini <>
2026-02-15 22:35:34 +0530

    swiftlint and swift format set up

---
+++ Swift-Concurrency/Swift-Concurrency/Swift_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ConcurrencyApp.swift
+//  Swift-Concurrency
+//
+//  Created by Sahil Saini on 15/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
